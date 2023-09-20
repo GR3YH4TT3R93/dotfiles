059da229399ad6f8cb62dd7bb6ba1d42848b49df
GR3YH4TT3R93 <76727386+GR3YH4TT3R93@users.noreply.github.com>
2023-09-19 22:38:05 -0700

    Updated install script

--- install.sh
+++ install.sh
@@ -1,63 +1,65 @@
 #!/data/data/com.termux/files/usr/bin/bash
 # Install script for My Termux Dotfiles
 
-## Install Nala Package Manager, Z Shell, Termux Clipboard, Git, GitHub CLI, Neovim, NodeJS, Python-pip, Ruby, wget, logo-ls, Timewarrior, Taskwarrior
+# Install Nala Package Manager, Z Shell, Termux Clipboard, Git, GitHub CLI, Neovim, NodeJS, Python-pip, Ruby, wget, logo-ls, Timewarrior, Taskwarrior
 
     apt update && apt install nala -y && nala install zsh termux-api gh neovim nodejs python-pip ruby wget logo-ls timewarrior taskwarrior -y
 
-### Install pynvim
+# Install pynvim
 
     python -m pip install pynvim
 
-### Install neovim npm package
+# Install neovim npm package
 
     npm install -g neovim
 
-### Install neovim gem package
+# Install neovim gem package
 
     gem install neovim
 
-## Set up GitHub auth
+# Set up GitHub auth
 
     gh auth login
 
-## Install MOTD
+# Install MOTD
 
     rm /data/data/com.termux/files/usr/etc/motd
     git clone https://github.com/GR3YH4TT3R93/termux-motd.git /data/data/com.termux/files/usr/etc/motd
     echo "/data/data/com.termux/files/usr/etc/motd/init.sh" >> /data/data/com.termux/files/usr/etc/zprofile
 
-## Install Oh My Zsh
+# Install Oh My Zsh
 
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-## Install Powerlevel10k
+# Install Powerlevel10k
 
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
 
-## Install Oh My Zsh plugins
+# Install Oh My Zsh plugins
 
-### Auto-Suggestions
+# Auto-Suggestions
 
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-### Completions
+# Completions
 
-    git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
+    git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions
 
-### History Substring Search
+# History Substring Search
 
     git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
 
-### Syntax Highlighting
+# Syntax Highlighting
 
     git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-### Git Flow Completions
+# Git Flow Completions
 
     git clone https://github.com/bobthecow/git-flow-completion ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/git-flow-completion
 
-### Zsh Vi Mode
+# Zsh Vi Mode
 
     git clone https://github.com/jeffreytse/zsh-vi-mode ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-vi-mode
 
+# reset .zshrc
+    mv .zshrc.pre-oh-my-zsh .zshrc
