63e636b292bd4b69253bec146f6cb10e2c43ce11
GR3YH4TT3R93 <76727386+GR3YH4TT3R93@users.noreply.github.com>
2024-01-26 01:22:00 -0800

    Feat: Add Copilot Lualine Plugin

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -2,6 +2,7 @@ return {
   "nvim-lualine/lualine.nvim",
   dependencies = {
     { "nvim-tree/nvim-web-devicons" },
+    { "AndreM222/copilot-lualine" },
   },
   config = function()
     require("lualine").setup({
@@ -27,10 +28,40 @@ return {
         lualine_a = { "mode" },
         lualine_b = { "branch", "diff", "diagnostics" },
         lualine_c = { { "filename", path = 0 } },
-        lualine_x = { "encoding", "fileformat", "filetype" },
-        lualine_y = { "progress" },
-        lualine_z = { "location" },
-      },
+        lualine_x = {
+          {
+            "copilot",
+            -- Default values
+            symbols = {
+              status = {
+                icons = {
+                  enabled = " ",
+                  sleep = " ",   -- auto-trigger disabled
+                  disabled = " ",
+                  warning = " ",
+                  unknown = " "
+                },
+                hl = {
+                  enabled = "#98c379",
+                  sleep = "#848b98",
+                  disabled = "#5c6370",
+                  warning = "#d19a66",
+                  unknown = "#848b98"
+                }
+              },
+              spinners = require("copilot-lualine.spinners").dots,
+              spinner_color = "#5c6370"
+            },
+            show_colors = true,
+            show_loading = true
+          },
+          "encoding",
+          "fileformat",
+          "filetype"
+        },
+      lualine_y = { "progress" },
+      lualine_z = { "location" },
+    },
       inactive_sections = {
         lualine_a = {},
         lualine_b = {},
