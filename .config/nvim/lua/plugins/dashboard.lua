e4d0d28dc02ac52d36b16b6a0b6bc9cd21bba829
GR3YH4TT3R93 <76727386+GR3YH4TT3R93@users.noreply.github.com>
2024-02-02 17:19:22 -0800

    Feat: Change Dashboard Shortcut Type To Numbers

--- .config/nvim/lua/plugins/dashboard.lua
+++ .config/nvim/lua/plugins/dashboard.lua
@@ -4,7 +4,7 @@ return {
     require("dashboard").setup({
       theme = "hyper", --  theme is doom and hyper default is hyper
       disable_move = false, --  default is false disable move keymap for hyper
-      shortcut_type = "letter", --  shorcut type "letter" or "number"
+      shortcut_type = "number", --  shorcut type "letter" or "number"
       change_to_vcs_root = true, -- default is false,for open file in hyper mru. it will change to the root of vcs
       config = { --  config used for theme
         header = {
