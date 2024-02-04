224923bf0cc73f7a0506e8b29f732ddc1c9913bc
GR3YH4TT3R93 <76727386+GR3YH4TT3R93@users.noreply.github.com>
2024-02-04 09:29:23 -0800

    Chore: Update Packages

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -28,4 +28,3 @@ vim.opt.clipboard:append({ "unnamed", "unnamedplus" })
 vim.lsp.set_log_level("OFF")
 vim.notify = require("notify")
 vim.o.pumblend = 1
-vim.g.perl_host_prog = "/data/data/com.termux/files/usr/bin/perl"
