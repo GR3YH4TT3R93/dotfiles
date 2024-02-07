20ccaa5a95498bbe7d11eeff86a61469541ec2cc
GR3YH4TT3R93 <76727386+GR3YH4TT3R93@users.noreply.github.com>
2024-02-07 03:22:55 -0800

    Feat: Add Zoxide

--- .termux/install.sh
+++ .termux/install.sh
@@ -244,12 +244,12 @@ else
   rm -rf README.md .git || error_exit "${RED}Failed to hide README.md.${ENDCOLOR}"
 fi
 
-echo -e "${GREEN}Time to install Nala Package Manager, Termux Clipboard, Neovim, NodeJS, Python-pip, Perl, Ruby, LuaRocks, LuaJIT, LazyGit, wget, gettext, logo-ls, ncurses-utils, libuv, Timewarrior, Taskwarrior, and htop!${ENDCOLOR}"
+echo -e "${GREEN}Time to install Nala Package Manager, Termux Clipboard, Neovim, NodeJS, Python-pip, Perl, Ruby, LuaRocks, LuaJIT, LazyGit, wget, gettext, logo-ls, ncurses-utils, libuv, Timewarrior, Taskwarrior, Zoxide and htop!${ENDCOLOR}"
 sleep 5
 
 # Install Nala Package Manager, Z Shell, Termux Clipboard, Git, GitHub CLI, Neovim, NodeJS, Python-pip, Ruby, wget, logo-ls, Timewarrior, Taskwarrior, htop
 apt update && apt install nala -y
-nala install termux-api gh neovim nodejs python-pip perl ruby luarocks luajit ripgrep fd lazygit wget gettext logo-ls ncurses-utils libuv timewarrior taskwarrior htop -y || error_exit "${RED}Failed to install packages.${ENDCOLOR}"
+nala install termux-api gh neovim nodejs python-pip perl ruby luarocks luajit ripgrep fd lazygit wget gettext logo-ls ncurses-utils libuv timewarrior taskwarrior zoxide htop -y || error_exit "${RED}Failed to install packages.${ENDCOLOR}"
 
 # Install pynvim, pnpm and neovim npm package, and neovim gem package
 pip install pynvim || error_exit "${RED}Failed to install pynvim.${ENDCOLOR}"
