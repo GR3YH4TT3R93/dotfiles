f35ffdc0da24eb66f1450111ba6d7a7dc7d3bcaa
GR3YH4TT3R93 <76727386+GR3YH4TT3R93@users.noreply.github.com>
2024-02-09 14:23:48 -0800

    Feat: Add Submodule Diff

--- .termux/autoinstall.sh
+++ .termux/autoinstall.sh
@@ -64,6 +64,7 @@ if [[ "$choice" == [Yy]* ]]; then
   git config --system gpg.format ssh
   git config --system user.signingkey ~/.ssh/id_ed25519.pub
   git config --system gpg.ssh.allowedSignersFile ~/.ssh/allowed_signers
+  git config --system diff.submodule log
   git config --system log.showSignature true
   git config --system commit.gpgsign true
   git config --system tag.gpgsign true
@@ -91,6 +92,7 @@ else
   git config --global gpg.format ssh
   git config --global user.signingkey ~/.ssh/id_ed25519.pub
   git config --global gpg.ssh.allowedSignersFile ~/.ssh/allowed_signers
+  git config --global diff.submodule log
   git config --global log.showSignature true
   git config --global commit.gpgsign true
   git config --global tag.gpgsign true
--- .termux/install.sh
+++ .termux/install.sh
@@ -65,6 +65,7 @@ if [[ "$choice" == [Yy]* ]]; then
   git config --system user.signingkey ~/.ssh/id_ed25519.pub
   git config --system gpg.ssh.allowedSignersFile ~/.ssh/allowed_signers
   git config --system merge.verifySignatures true
+  git config --system diff.submodule log
   git config --system log.showSignature true
   git config --system commit.gpgsign true
   git config --system tag.gpgsign true
@@ -93,6 +94,7 @@ else
   git config --global user.signingkey ~/.ssh/id_ed25519.pub
   git config --global gpg.ssh.allowedSignersFile ~/.ssh/allowed_signers
   git config --global merge.verifySignatures true
+  git config --global diff.submodule log
   git config --global log.showSignature true
   git config --global commit.gpgsign true
   git config --global tag.gpgsign true
