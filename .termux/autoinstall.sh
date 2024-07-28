781898c2dc4e5684540e70e8a2de23c51ba0087d
GR3YH4TT3R93 <76727386+GR3YH4TT3R93@users.noreply.github.com>
2024-07-28 08:55:08 -0700

    Fix: Ensure yq is installed

--- .termux/autoinstall.sh
+++ .termux/autoinstall.sh
@@ -118,12 +118,12 @@ else
   echo -e "${GREEN}Git credentials configured globally.${ENDCOLOR}"
 fi
 
-echo -e "${GREEN}Time to install Nala Package Manager, Termux Clipboard, Neovim, Lua Language Server, Stylua, rust, rust-analyzer, NodeJS, Python-pip, Perl, Ruby, SQLite, LuaRocks, LuaJIT, LazyGit, Ranger, RipGrep, fd, wget, gettext, logo-ls, ncurses-utils, libuv, Timewarrior, Taskwarrior, Zoxide, Zellij, tmux, htop, and yazi!${ENDCOLOR}"
+echo -e "${GREEN}Time to install Nala Package Manager, Termux Clipboard, Neovim, Lua Language Server, Stylua, rust, rust-analyzer, NodeJS, Python-pip, Perl, Ruby, SQLite, LuaRocks, LuaJIT, LazyGit, Ranger, RipGrep, fd, yq, wget, gettext, logo-ls, ncurses-utils, libuv, Timewarrior, Taskwarrior, Zoxide, Zellij, tmux, htop, and yazi!${ENDCOLOR}"
 sleep 5
 
 # Install Nala Package Manager, Z Shell, Termux Clipboard, Neovim, NodeJS, Python-pip, Ruby, wget, logo-ls, Timewarrior, Taskwarrior, htop
 pkg update && pkg install nala -y
-nala install termux-api gh neovim lua-language-server stylua rust rust-analyzer nodejs python-pip perl ruby sqlite luarocks luajit ripgrep fd lazygit ranger wget gettext logo-ls ncurses-utils libuv timewarrior taskwarrior zoxide zellij tmux htop yazi -y || error_exit "${RED}Failed to install packages.${ENDCOLOR}"
+nala install termux-api gh neovim lua-language-server stylua rust rust-analyzer nodejs python-pip perl ruby sqlite luarocks luajit ripgrep fd yq lazygit ranger wget gettext logo-ls ncurses-utils libuv timewarrior taskwarrior zoxide zellij tmux htop yazi -y || error_exit "${RED}Failed to install packages.${ENDCOLOR}"
 
 # Install pynvim, pnpm and neovim npm package, and neovim gem package
 pip install pynvim || error_exit "${RED}Failed to install pynvim.${ENDCOLOR}"
